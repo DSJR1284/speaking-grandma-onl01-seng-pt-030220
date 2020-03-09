4307b95836f8b87396eb9ee8f76d01b769decdfd
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-09 00:19:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,6 +10,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase==phrase.upcase
     return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end 
   
