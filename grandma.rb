429830d9b12f2795c1b00983073be043a0f2f762
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-09 00:15:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 #if its isnt grandma cant hear you 
 #Output grandma response speak , 1938,i love you
 
-phrase = "I LOVE YOU GRANDMA!"
+
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
