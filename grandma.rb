ed610b56327c84d5681cad829118fe17a50fa5a4
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-09 00:07:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,16 +3,6 @@
 #if its isnt grandma cant hear you 
 #Output grandma response speak , 1938,i love you
 
-def speak_to_grandma(phrase)
- answer = "HUH?! SPEAK UP SONNY!"
-  if phrase == "I LOVE YOU YOU GRANDMA!"
-    print "I LOVE YOU TOO PUMPKIN!"
-  else if phrase == phrase.uppercase
-    print ="NO, NOT SINCE 1938!"
-  else 
-    print answer 
-  end
-end 
-  
+
     
  
